866571c084694cb15ec9d2f30136b117e4753a2e
ThingPlug <skt.thingplug@gmail.com>
2017-09-27 21:24:14 +0900

    Update Configuration.h
    changed mqtt host

--- RaspberryPi/samples/Configuration.h
+++ RaspberryPi/samples/Configuration.h
@@ -10,7 +10,7 @@
 #define _CONFIGURATION_H_
 
 //#define MQTT_HOST                           "onem2m.sktiot.com"
-#define MQTT_HOST                           "thingplugtest.sktiot.com" // Demoday
+#define MQTT_HOST                           "mqtt.sktiot.com" // Demoday
 #define MQTT_PORT                           1883
 #define MQTT_SECURE_PORT                    8883
 #define MQTT_KEEP_ALIVE                     120
@@ -22,7 +22,7 @@
 
 #define ONEM2M_NODEID                       "" // ex : 1.2.481.1.999.203.000100
 //#define ONEM2M_TO                           "/ThingPlug" // onem2m.sktiot.com
-#define ONEM2M_TO                           "/ThingPlug/v1_0" // thingplugtest.sktiot.com
+#define ONEM2M_TO                           "/ThingPlug/v1_0" // mqtt.sktiot.com
 #define ONEM2M_PASSCODE                     "123456" // ex : 123456
 #define ONEM2M_RI                           "12345"
 
